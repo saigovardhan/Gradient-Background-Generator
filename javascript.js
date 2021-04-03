46ad4977540ff7e7cb5f31d4f5637d200f8f1b98
saigovardhan <veggalam.saigovardhan@gmail.com>
2021-04-03 19:40:55 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var h3 = document.querySelector("h3");
+var body = document.getElementById("gradient");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+function changeBG(){
+  body.style.background =
+  "linear-gradient(to right,"
+  +color1.value
+  +","
+  +color2.value
+  +")";
+}
+
+color1.addEventListener("input", changeBG)
+color2.addEventListener("input", changeBG)
